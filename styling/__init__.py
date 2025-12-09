b3a1e038b5495b3e6f69edfd729f20ca3c427704
Lianefiligrane56 <lianesgithub@use.startmail.com>
2025-12-09 07:04:57 +0100

    Add __init__.py files for Python package imports

---
+++ styling/__init__.py
@@ -0,0 +1,3 @@
+# Symbiotic Styling Package
+from .symbiotic_styling import style_dataframe, display_section, display_markdown
+from .symbiotic_sql_data_styling import style_dataframe as style_sql_dataframe
